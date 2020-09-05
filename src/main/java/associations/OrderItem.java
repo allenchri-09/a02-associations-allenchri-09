778c26383c68803bc3fe5178e0f8203578a188a4
allenchri-09 <62294373+allenchri-09@users.noreply.github.com>
2020-09-05 10:16:39 -0400

    commit1

--- src/main/java/associations/OrderItem.java
+++ src/main/java/associations/OrderItem.java
@@ -1,13 +1,23 @@
 package associations;
 
 public class OrderItem {
+    private Product prod;
+    private int quantity;
+    private double price;
+
 
   // A constructor that initializes the product and quantity
-
+    public OrderItem(Product prod, int quantity, int price){
+        this.prod = prod;
+        this.quantity = quantity;
+        this.price = price;
+    }
   // getProduct() - Returns a reference to the Product object purchased
+    public Product getProduct(){return prod;}
 
   // getQuantity() - Returns an integer indicating how many products were purchased
+    public int getQuantity(){return quantity;}
 
   // getPrice() - Returns the price of this item, based on the unit price of the product and the quantity
-
+    public double getPrice(){return price*quantity;}
 }
